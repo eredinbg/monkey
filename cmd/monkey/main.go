cde3adedd8d8b6663d4877ec03aa1f5a9315f0f9
Nikita Fomin <eredinbg@gmail.com>
2023-08-24 00:28:57 +0400

    Start REPL

---
+++ cmd/monkey/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/eredinbg/monkey/cmd/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
